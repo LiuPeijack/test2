1fe3cafdef53d5ea15aae90026fe0fb257733809
liupeijia <1780822747@qq.com>
2020-09-18 17:04:48 +0800

    test

--- src/main/java/com/example/demo3/Demo2Application.java
+++ src/main/java/com/example/demo3/Demo2Application.java
@@ -2,9 +2,11 @@ package com.example.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Demo2Application {
+	String test = "test";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
