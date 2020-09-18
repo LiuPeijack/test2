dc3aa30868181b40cb1b045591c11d0a2ae1e79d
liupeijia <1780822747@qq.com>
2020-09-18 17:32:17 +0800

    dev1

--- src/main/java/com/example/demo3/Demo2Application.java
+++ src/main/java/com/example/demo3/Demo2Application.java
@@ -8,6 +8,8 @@ import org.springframework.http.converter.json.GsonBuilderUtils;
 public class Demo2Application {
 	String test = "test";
 
+	String dev1 = "dev";
+
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
